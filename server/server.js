6abc2233d61afd8298f48f6b4b7621baaf3839f6
synoize <suryavanshivam@gamil.com>
2025-10-20 20:30:53 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
-        "https://appliancesakhi.in"
+        "https://appliancesakhi.in",
+        "https://www.appliancesakhi.in"
     ],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
